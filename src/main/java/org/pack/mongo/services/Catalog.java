d4c6236ed365c328d0156b4b4b8ca8269832178b
Adithya Narayan <Aditya.Narayana@fisglobal.com>
2017-08-07 20:03:28 +0530

    mongo model update

--- src/main/java/org/pack/mongo/services/Catalog.java
+++ src/main/java/org/pack/mongo/services/Catalog.java
@@ -1,9 +1,26 @@
 package org.pack.mongo.services;
 
-public class Catalog {
+import java.io.Serializable;
+import com.mongodb.BasicDBObject;
 
-	public static void main(String[] args) {
+public class Catalog extends BasicDBObject implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String catalogId;
+	private String journal;
+	private String publisher;
+	private String edition;
+	private String title;
+	private String author;
 
+	public Catalog() {
 	}
 
+	public Catalog(String catalogId, String journal, String publisher, String edition, String title, String author) {
+		this.catalogId = catalogId;
+		this.journal = journal;
+		this.publisher = publisher;
+		this.edition = edition;
+		this.title = title;
+		this.author = author;
+	}
 }
--- src/main/java/org/pack/mongo/services/CreateMongoDBDocumentModel.java
+++ src/main/java/org/pack/mongo/services/CreateMongoDBDocumentModel.java
@@ -1,9 +1,46 @@
 package org.pack.mongo.services;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+
 public class CreateMongoDBDocumentModel {
 
 	public static void main(String[] args) {
+		
+		MongoClient mongoClient = null;
+		try {
+			// Create a mongo client
+			mongoClient = new MongoClient(Arrays.asList(new ServerAddress("localhost", 27017)));
 
+			// Get defauly DB by the name 'local'
+			MongoDatabase mongoDB = mongoClient.getDatabase("local");
+		}
+		finally {
+			mongoClient.close();
+		}
+		
+		CreateMongoDBDocumentModel createMongoDBDocumentModel = new CreateMongoDBDocumentModel();
+		createMongoDBDocumentModel.insertMultipleRecords();
 	}
 
+	private void insertMultipleRecords() {
+		Catalog adamsCatalog = new Catalog("Adams", "Journal1", "Adams publisher", "2017", "King Adams", "Adams");
+		Catalog catalog24 = new Catalog("24", "Journal2", "24 publisher", "2017", "King 24", "24");
+		
+		Document documents = new Document();
+		documents.append("adams", adamsCatalog);
+		documents.append("24", catalog24);
+		
+		List<Document> documentList = new LinkedList<>();
+		documentList.add(documents);
+		
+		
+	}
 }
